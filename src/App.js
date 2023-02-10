63cd6c067d28c6e81210b502ae387a8b21eda85b
Do Thuy <thuydo2303@gmail.com>
2023-02-11 05:09:08 +0900

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css';
-import { DISHES  } from './shared/dishes';
 import Main from './components/MainComponent';
 class App extends Component {
   render() {
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
-import DishDetail from "./DishDetailComponent";
 
 class Menu extends Component {
 
